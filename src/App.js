8bf7a154662aa43e713f937863bf19a7e0dc3889
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-09-24 23:23:43 -0400

    Add recursive fetching to save all batches of images

--- src/App.js
+++ src/App.js
@@ -1,78 +1,57 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-function fetchPhotos() {
-  const url =
-    'https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&gcmtype=file&gcmtitle=Category:Featured_pictures_of_landscapes&prop=imageinfo&gcmlimit=50&iiprop=url|extmetadata&format=json&origin=*';
-  let dataArray;
-  let continueFetch = true;
-
-  console.log('Fetching photos with function');
-
-  return fetch(url).then((response) => response.json());
-  /* TODO: Check if returned json has a continue
-    .then((data) => {
-      if (data.contine.gcmcontinue === true) {
-        url = url + "&gcmcontinue=" + data.continue.gcmcontinue;
-      } else {
-        continueFetch = false;
-      }
-    }
-  */
-
-  /*
-    TODO:
-    Query recursively for more responses, until continue is no longer present
-    if data.continue === true (meaning there is a continue property present, and there are more responses to fetch)
-      grab data.continue.gcmcontinue
-        add gcmcontinue to end of url with "&gcmcontinue=(gcmcontinue string)"
-      concat new array (array1.concat(array2)) from next fetch with old array, and return full array
-  */
-
-  /*
-  ORIGINAL FETCH:
-  if (continueFetch) {
-    // finish last small step
-    // end recursion
-  } else {
-    // call fetchPhotos again
-  }
-  */
-
-  /*
-  returnfetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-
-      // Change object of photos into an array
-      dataArray = Object.values(data.query.pages);
-      //photoSetter(dataArray);
-      console.log('Data array in fetch: ', dataArray);
-    })
-    .catch((err) => console.error('Error:', err));
-
-  console.log('DataArray: ' + dataArray);
-  return dataArray;
-
-  */
-}
-
 function App() {
   const [photos, setPhotos] = useState([]);
+  const [fetchIsFinished, setFetchIsFinished] = useState(false); // TODO: Find way to replace this so useEffect only runs once
 
   useEffect(() => {
-    fetchPhotos() //get returned promise from fetch
-      .then((data) => {
-        let dataArray = Object.values(data.query.pages); // Change object of photos into an array
-        setPhotos(dataArray); // Save photos' info in state
-      })
-      .catch((err) => console.error('Error:', err));
+    let url =
+      'https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&gcmtype=file&gcmtitle=Category:Featured_pictures_of_landscapes&prop=imageinfo&gcmlimit=500&iiprop=url|extmetadata&format=json&origin=*';
+
+    if (!fetchIsFinished) {
+      fetchPhotos(url, setPhotos, setFetchIsFinished) //get returned promise from fetch
+        .catch((err) => console.error('Error:', err));
+    }
   }, []);
 
-  console.log('Photos state outside effect: ', photos);
+  // Show loading text if no photos saved yet
+  if (photos === undefined || photos.length == 0) {
+    return <div className="App">Fetching images...</div>;
+  }
 
   return <div className="App"></div>;
+}
+
+function fetchPhotos(url, setPhotos, setFetchIsFinished) {
+  // Test for no continue:  'https://commons.wikimedia.org/w/api.php?action=query&generator=categorymembers&gcmtype=file&gcmtitle=Category:Documents signed by Boris Yeltsin&prop=imageinfo&gcmlimit=50&iiprop=url|extmetadata&format=json&origin=*';
+
+  // TODO:
+  // Fetch any subcategories and images held in subcategories
+
+  // TODO:
+  // Make sure errors are caught for each call of fetch (and not just first function call in useEffect)?
+
+  //Query recursively to fetch all images (until continue property is no longer present)
+  return fetch(url)
+    .then((response) => response.json())
+    .then(function (data) {
+      if ('continue' in data) {
+        // Add new data to array of data
+        // Combine earlier data with new array of data & save in state
+        let newData = Object.values(data.query.pages);
+        setPhotos((prevState) => prevState.concat(newData)); // combine earlier data with new array of data & save in state
+        // Call fetch again with new url
+        url = url + '&gcmcontinue=' + data.continue.gcmcontinue;
+        fetchPhotos(url, setPhotos, setFetchIsFinished);
+      } else {
+        //End recursion and add last values to sstate
+        console.log('No more continues');
+        let newData = Object.values(data.query.pages);
+        setPhotos((prevState) => prevState.concat(newData)); // combine earlier data with new array of data & save in state
+        setFetchIsFinished(true);
+      }
+    });
 }
 
 export default App;
