09b2d6031f5dfa69c3a25a3eb0e357a28219e123
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-10-23 22:57:30 -0500

    Add info button and adjust overall button layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import rightArrow from '../assets/right-arrow.svg';
 import leftArrow from '../assets/left-arrow.svg';
 import playButton from '../assets/play-icon.svg';
 import pauseButton from '../assets/pause-icon.svg';
+import infoIcon from '../assets/info.svg';
 
 import Timer from './Timer';
 
@@ -16,37 +17,42 @@ const Footer = ({ handleNextPhoto }) => {
 
   return (
     <footer>
-      <Timer runTimer={runTimer} />
-      <div className="button-container">
-        <button className="arrowButton">
-          <img
-            src={leftArrow}
-            alt="Last image"
-            onClick={() => handleNextPhoto('Back')}
-          />
-        </button>
-        <button className="playButton">
-          {runTimer ? (
+      <button className="infoButton">
+        <img src={infoIcon} alt="Open image information" />
+      </button>
+      <div className="button-container vertical">
+        <Timer runTimer={runTimer} />
+        <div className="button-container">
+          <button className="arrowButton">
             <img
-              src={pauseButton}
-              alt="Pause timer"
-              onClick={handleToggleTimer}
+              src={leftArrow}
+              alt="Last image"
+              onClick={() => handleNextPhoto('Back')}
             />
-          ) : (
+          </button>
+          <button className="playButton">
+            {runTimer ? (
+              <img
+                src={pauseButton}
+                alt="Pause timer"
+                onClick={handleToggleTimer}
+              />
+            ) : (
+              <img
+                src={playButton}
+                alt="Start timer"
+                onClick={handleToggleTimer}
+              />
+            )}
+          </button>
+          <button className="arrowButton">
             <img
-              src={playButton}
-              alt="Start timer"
-              onClick={handleToggleTimer}
+              src={rightArrow}
+              alt="Next image"
+              onClick={() => handleNextPhoto('Next')}
             />
-          )}
-        </button>
-        <button className="arrowButton">
-          <img
-            src={rightArrow}
-            alt="Next image"
-            onClick={() => handleNextPhoto('Next')}
-          />
-        </button>
+          </button>
+        </div>
       </div>
     </footer>
   );
