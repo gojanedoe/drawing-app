2578f34a6e3020315ead07d1ff5b22936ffac4de
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-10-30 02:30:50 -0500

    Handle page change with form submission and add inputs

--- src/components/CategorySelect.js
+++ src/components/CategorySelect.js
@@ -1,21 +1,33 @@
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const CategorySelect = () => {
+  let history = useHistory();
+  let formIsValid = false;
+
   const submitHandler = (event) => {
     event.preventDefault();
+    history.push('/app');
   };
 
   return (
-    <form>
-      <Link to="/app" className="btn btn-light">
+    <form onSubmit={submitHandler}>
+      <label className="btn btn-light">
+        <input type="checkbox" value="1" />
+        <span>People</span>
+      </label>
+      <label className="btn btn-light">
+        <input type="checkbox" value="1" />
+        <span>Animals</span>
+      </label>
+      <label className="btn btn-light">
+        <input type="checkbox" value="1" />
+        <span>Landscapes</span>
+      </label>
+      {/* <div className="container text-center"> */}
+      <button type="submit" className="btn btn-primary">
         Start
-        {/* <input
-          type="submit"
-          onClick={submitHandler}
-          value="Start"
-          className="btn btn-light"
-        ></input> */}
-      </Link>
+      </button>
+      {/* </div> */}
     </form>
   );
 };
