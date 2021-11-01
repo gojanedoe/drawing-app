9b776b1bd78a0d625a75679a6c9dba76654dce54
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-11-01 13:46:40 -0500

    Fix console errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <Link className="site-title" to="/" exact>
+      <Link className="site-title" to="/">
         Draw This!
       </Link>
       <div className="button-container">
